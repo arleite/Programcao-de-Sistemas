9a108790b40a1c3ac1a768d3cfa299c6d5a60a4a
TiagoSRangel <71654008+TiagoSRangel@users.noreply.github.com>
2021-05-11 12:16:09 +0100

    Add files via upload

--- MC.c
+++ MC.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 int main()
 {
+    int idFuncionario;
     int contador=0;
-    char contadorString[1000];
+    char contadorString[100];
+    char iString[100];
     float salarioF;
     int a=1, opc1=0, opc2=0, opc3=0, opc4=0, opc5=0, opc6=0, opc7=0, check;
     char caminhoF[] = "Files/Funcionarios/";
     char* dirnameFuncionarios = "Funcionarios";
+    char* result;
+    char linha[100];
     char* dirnameFile = "Files";
     char* nomeF[50], estatutoF[50];
 
     FILE *FileF;
-    FILE *ContadorFuncionarios = NULL;
+    FILE *ContadorFuncionarios;
+
+   //funções
+    void contadorInteiro (){
+        ContadorFuncionarios = fopen ("Files/Funcionarios/ContadorFuncionarios.txt", "r");
+        fscanf(ContadorFuncionarios, "%d", &contador);}
+
+
 
 
     while (a==1){
@@ -58,7 +69,6 @@ int main()
                 {
                 case 1:
                     check = mkdir("Files/Funcionarios", dirnameFile, 0777);
-                    if (!check) { printf("Directory created\n"); } else { printf("Unable to create directory\n"); }
 
                     printf("Nome: ");
                     scanf("%s", &nomeF);
@@ -67,7 +77,6 @@ int main()
                     printf("Salario: ");
                     scanf("%f", &salarioF);
 
-
                     if(!ContadorFuncionarios)
                     {
                        ContadorFuncionarios = fopen("Files/Funcionarios/ContadorFuncionarios.txt" ,"w");
@@ -75,49 +84,98 @@ int main()
                        fclose(ContadorFuncionarios);
                     }
 
-                    ContadorFuncionarios = fopen ("Files/Funcionarios/ContadorFuncionarios.txt", "r");
-                    fscanf(ContadorFuncionarios, "%d", &contador);
+                    contadorInteiro(); //função para ir buscar o valor inteiro do ficheiro contador
 
-                    sprintf(contadorString, "%d",contador);
+                    sprintf(contadorString, "%d",contador); //passar o valor do contador para string, ou seja, contadorString
 
-
-                    strcat(caminhoF, contadorString);
+                    strcat(caminhoF, contadorString); //junta strings
                     strcat(caminhoF, ".txt");
 
-
                     FileF = fopen(caminhoF, "w");
-                    fprintf(FileF, "%s\n%s\n%f\n", nomeF, estatutoF, salarioF);
+                    fprintf(FileF, "%s\n%s\n%.2f\n", nomeF, estatutoF, salarioF);
                     fclose(FileF);
 
                     contador += 1;
-                    strcpy(caminhoF, "Files/Funcionarios/");
+
+                    strcpy(caminhoF, "Files/Funcionarios/"); //limpar variavel, copia o segundo parametro
 
                     ContadorFuncionarios = fopen ("Files/Funcionarios/ContadorFuncionarios.txt", "w");
                     fprintf(ContadorFuncionarios, "%d", contador);
-
-                    //int atoi(const char* contadorString);
-
-
-
-
-
-
-
-
                     fclose(ContadorFuncionarios);
 
+                    break;
+
+                case 2:
+                    printf("Introduza o Id do funcionário: ");
+                    scanf("%d", &idFuncionario);
+
+                    sprintf(iString, "%d", idFuncionario);
+                    strcat(caminhoF, iString);
+                    strcat(caminhoF, ".txt");
+
+                    FileF = caminhoF;
+
+                    if(FileF != NULL)
+                    {
+                       remove(caminhoF);
+                       printf("Funcionario eliminado com sucesso!");
+                    }
+                    else
+                    {
+                        printf("O funcionario nao existe!");
+                    }
 
 
 
-
-
-                    //fprintf("%s", contador);
 
                     break;
 
-                case 0:
+                case 3:
 
-                    break;
+                    contadorInteiro();
+
+                    for(int i = 0; contador > i;i++)
+                    {
+
+                        sprintf(iString, "%d", i);
+
+                        strcat(iString, ".txt");
+
+                        strcat(caminhoF, iString);
+
+                        FileF = fopen(caminhoF, "r");
+
+                        int k = 0;
+
+                         while (!feof(FileF))
+                        {
+                            result = fgets(linha, 100, FileF);
+
+                            if(result)
+                            {
+                                if(k==0)
+                                {
+                                    printf("\n--------------------\n\n");
+                                    printf("Id: %d\n", i);
+                                    printf("Nome: %s", linha);
+                                }
+                                if(k==1)
+                                {
+                                    printf("Estatuto: %s", linha);
+                                }
+                                if(k==2)
+                                {
+                                    printf("Salario: %s", linha);
+                                }
+                            }
+
+                            k++;
+                        }
+
+                        fclose(FileF);
+
+                        strcpy(caminhoF, "Files/Funcionarios/");
+                    }
 
                     break;
 
