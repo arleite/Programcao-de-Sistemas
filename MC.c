70b4a0695afc05011382f121ef847cac3bafa1f4
TiagoSRangel <71654008+TiagoSRangel@users.noreply.github.com>
2021-05-23 16:07:37 +0100

    Add files via upload

--- MC.c
+++ MC.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int a=1, b=1, opcao, letras = 0, contador = 1, id;
+    int a=1, opcao, letras = 0, contador = 1, id, b=1;
     char nomeF[50], estatutoF[50];
     float salarioF;
     char* dirnameFuncionarios = "Funcionarios";
@@ -22,8 +22,6 @@ int main()
     FILE *fileContadorFuncionario;
     FILE *fileContadorStock;
     FILE *fileContadorCliente;
-
-
 
    //funções
     void contadorInteiroFuncionarios(){
@@ -44,7 +42,7 @@ int main()
         mkdir(dirnameFile, 0777);
 
         printf("\n--------------Menu----------------\n");
-        printf("1 - Gestao da Empresa\n");
+        printf("\n1 - Gestao da Empresa\n");
         printf("2 - Cliente\n");
         printf("\n0 - Sair\n");
         printf("Selecione uma opcao: ");
@@ -80,9 +78,12 @@ int main()
                 printf("\nSelecione uma opcao: ");
                 scanf("%d", &opcao);
 
+                system("cls");
+
                 switch (opcao)
                 {
                 case 1:
+                    printf("Adicionar o funcionario\n");
                     printf("Nome: ");
                     scanf("%s", &nomeF);
 
@@ -96,18 +97,41 @@ int main()
                         if (strlen(nomeF) != letras)
                         {
                             letras = 0;
-                            printf("Introduza um nome!\n");
+                            printf("Introduza apenas letras!\n");
                             printf("Nome: ");
                             scanf("%s", &nomeF);
                         }
                         else
                         {
+                            letras = 0;
                             b = 0;
                         }
                     }
 
                     printf("Estatuto: ");
                     scanf("%s", &estatutoF);
+                    b = 1;
+                    while (b == 1) //verificar que só introduziu letras
+                    {
+                        for (int i = 0; estatutoF[i] != '\0'; i++)
+                        {
+                            if (isalpha(estatutoF[i]))
+                                letras++;
+                        }
+                        if (strlen(estatutoF) != letras)
+                        {
+                            letras = 0;
+                            printf("Introduza apenas letras!\n");
+                            printf("Estatuto: ");
+                            scanf("%s", &estatutoF);
+                        }
+                        else
+                        {
+                            letras = 0;
+                            b = 0;
+                        }
+                    }
+
                     printf("Salario: ");
                     scanf("%f", &salarioF);
 
@@ -140,7 +164,8 @@ int main()
                     break;
 
                 case 2:
-                    printf("\nPressione 0 para voltar.\n");
+                    printf("Remover funcionario\n");
+                    printf("Pressione 0 para voltar.\n");
                     printf("Introduza o Id do funcionario: ");
                     scanf("%d", &id);
 
@@ -419,12 +444,59 @@ int main()
                     break;
 
                 case 4:
-                    printf("Qual o id do produto que pretende eliminar");
+                    printf("Qual o id do produto que pretende eliminar\n");
+                    scanf("%d", &id);
+
+                    sprintf(iString, "%d", id); //passar um numero inteiro para char
+                    strcpy(caminho, "Files/Produtos/"); //limpar variavel, copia o segundo parametro
+                    strcat(caminho, iString); //junta strings
+                    strcat(caminho, ".txt");
+
+                    fileF = fopen(caminho, "r");
+
+                    printf("Produto\n");
+
+                    if(fileF)
+                    {
+                        int k = 0;
+                        while (!feof(fileF))
+                        {
+                            result = fgets(linha, 100, fileF);
+                            if(result)
+                            {
+                                if(k==0)
+                                {
+                                    printf("Ingrediente: %s", linha);
+                                }
+
+                                if(k==1)
+                                {
+                                    printf("Stock: %s\n", linha);
+                                }
+                            }
+                            k++;
+                        }
+                    fclose(fileF);
+                    }
+
+                    int aux;
+
+                    printf("Tem a certeza que pretende eliminar este produto\n");
+                    printf("Sim - 1\n");
+                    printf("Nao - 2\n");
+                    scanf("%d", &aux);
+
+                    if(aux == 1)
+                    {
+                        remove(caminho);
+                    }
 
 
-                    printf("O produto que pretende eliminar é esse?");
 
                 }
+
+                break;
+
             break;
 
             case 3:
@@ -450,6 +522,7 @@ int main()
                 {
                     printf("Nome: ");
                     scanf("%s", &nomeC);
+
                     b=1;
                     letras = 0;
                     while (b == 1) //verificar que só introduziu letras
@@ -462,7 +535,7 @@ int main()
                         if (strlen(nomeC) != letras)
                         {
                             letras = 0;
-                            printf("Introduza um nome!\n");
+                            printf("Introduza apenas letras!\n");
                             printf("Nome: ");
                             scanf("%s", &nomeC);
                         }
@@ -474,24 +547,6 @@ int main()
 
                     printf("Numero de telemovel: ");
                     scanf("%d", &numeroTC);
-                    int aux;
-                    b=1;
-                    while (b == 1) //verificar que só introduziu letras
-                    {
-                        aux = numeroTC/100000000;
-                        if (!isdigit(numeroTC) && aux > 1)
-                        {
-                            aux=0;
-                            printf("\nIntroduza um numero valido!\n");
-                            printf("%d", aux);
-                            printf("Numero de telemovel: ");
-                            scanf("%d", &numeroTC);
-                        }
-                        else
-                        {
-                            b = 0;
-                        }
-                    }
 
                     printf("NIF: ");
                     scanf("%d", &nifC);
@@ -568,6 +623,7 @@ int main()
                         }
                     }
                 }
+
             break;
 
             case 5:
